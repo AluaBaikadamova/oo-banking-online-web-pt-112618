8bb15e6ea80479bba641c36276e2637f2cc20220
Alua Baikadamova <abaikad00@gmail.com>
2019-01-29 03:14:46 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -3,7 +3,7 @@ class BankAccount
 
   attr_accessor :balance, :status
     attr_reader :name
-def initialize(name,balance=1000,status="open")
+def initialize(name,balance,status="open")
   @name=name
   @balance=balance
   @status=status
