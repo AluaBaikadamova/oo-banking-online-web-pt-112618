64cc1d1b8d16f105aac55e14ccb14d2239f76ea5
Alua Baikadamova <abaikad00@gmail.com>
2019-01-29 02:13:21 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,8 +1,15 @@
 class BankAccount
-  attr_accessor :name
+  attr_accessor :name, :balance, :status
 def initialize(name,balance=1000,status='open')
   @name=name
   @balance=balance
   @status=status
+end
+def deposit(sum)
+@balance=@balance+sum
+end
+  
+def display_balance
+  puts "Your balance is $#{@balance}.""
 end
 end
