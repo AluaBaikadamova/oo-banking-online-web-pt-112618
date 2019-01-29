c0c9d8a3a886fe768204de50ea175278703c7ed9
Alua Baikadamova <abaikad00@gmail.com>
2019-01-29 02:57:56 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,25 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+  attr_reader :name
+def initialize(sender,receiver, amount,status="pending")
+  @sender=sender
+  @receiver=receiver
+  @amount=amount
+  
+  @status=status
+end
+def deposit(summ)
+@balance=@balance+summ
+end
+
+def display_balance
+  puts "Your balance is $#{@balance}."
+end
+
+def valid?
+ if (@status=="open")&&(balance>0)
+   return true
+ else
+   return false
+ endr code here
 end
